5bf85fb7a46f2643ccaf4a772ecb2462830fdebc
Shu12388y <spl.sp999@gmail.com>
2024-08-29 00:15:16 +0530

    updated the files

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe())
   const config = new DocumentBuilder()
     .setTitle('TODO REST API')
@@ -13,7 +14,14 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/swagger', app, document,{
+    customCssUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs:[
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle,js',
+      'https://cdnjs.cloudflarw.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-present.js'
+    ]
+  });
   await app.listen(3000);
 }
 bootstrap();
